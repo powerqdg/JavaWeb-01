82f857ba6a671943a88d4e589719d0895305a310
dgshin <powerqdg@gmail.com>
2023-12-11 17:26:47 +0900

    MemberListServlet DB연동

--- src/lesson02/servlets/MemberListServlet.java
+++ src/lesson02/servlets/MemberListServlet.java
@@ -2,6 +2,11 @@ package lesson02.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.servlet.GenericServlet;
 import javax.servlet.ServletException;
@@ -15,12 +20,39 @@ public class MemberListServlet extends GenericServlet {
 
 	@Override
 	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html;charset=UTF-8");
-		PrintWriter out = response.getWriter();
-		out.println("<html><head><title>회원목록</title>");
-		out.println("<body>");
-		out.println("<h1>회원목록</h1>");
-		out.println("</body></html>");
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+			conn = DriverManager.getConnection(
+					"jdbc:oracle:thin:@localhost:1521:orcl", 
+					"scott", 
+					"tiger");
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery("SELECT MNO, EMAIL, MNAME, CRE_DATE, MOD_DATE FROM MEMBERS ORDER BY MNO");
+			
+			response.setContentType("text/html;charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			out.println("<html><head><title>회원목록</title>");
+			out.println("<body>");
+			out.println("<h1>회원목록</h1>");
+			while (rs.next()) {
+				out.println(rs.getInt("MNO") + ", " + 
+							rs.getString("EMAIL") + ", " + 
+							rs.getString("MNAME") + ", " +
+							rs.getDate("CRE_DATE") + ", " +
+							rs.getDate("MOD_DATE") + "<br>"); 
+			}
+			out.println("</body></html>");
+		} catch (Exception e) {
+			new ServletException(e);
+		} finally {
+			try { if (rs != null) rs.close(); } catch (SQLException e) {}
+			try { if (stmt != null) stmt.close(); } catch (SQLException e) {}
+			try { if (conn != null) conn.close(); } catch (SQLException e) {}
+		}
 	}
 
 }
