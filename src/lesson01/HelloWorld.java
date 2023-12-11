2ceaad9966ea74cffee306cba572c06390c90fdd
dgshin <powerqdg@gmail.com>
2023-12-11 16:41:57 +0900

    GenericServlet 상속하여 서블릿 구현

--- src/lesson01/HelloWorld.java
+++ src/lesson01/HelloWorld.java
@@ -2,42 +2,19 @@ package lesson01;
 
 import java.io.IOException;
 
-import javax.servlet.Servlet;
-import javax.servlet.ServletConfig;
+import javax.servlet.GenericServlet;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 
 @WebServlet("/hello")
-public class HelloWorld implements Servlet {
-	ServletConfig config;
-	
-	@Override
-	public void init(ServletConfig config) throws ServletException {
-		System.out.println("init");
-		this.config = config;
-	}
+public class HelloWorld extends GenericServlet {
+	private static final long serialVersionUID = 1L;
 
 	@Override
 	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
 		System.out.println("service");
 	}
 	
-	@Override
-	public ServletConfig getServletConfig() {
-		System.out.println("getServletConfig");
-		return config;
-	}
-
-	@Override
-	public void destroy() {
-		System.out.println("destroy");
-	}
-
-	@Override
-	public String getServletInfo() {
-		System.out.println("getServletInfo");
-		return null;
-	}
 }
